0ed56fce98a395ecdf482a9c3b3ac5ef874213dc
janinejad <m.tarjomeh@gmail.com>
2024-11-08 21:10:43 +0330

    update6

--- config/settings.py
+++ config/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 #
 # DATABASES = {
 #     'default': {
@@ -110,16 +110,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'amordadvlv_db',
-        'USER': 'postgres',
-        'PASSWORD': 'adOt4NwaCM64cWQesnOl',
-        'HOST': 'amordad-fei-service',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'amordadvlv_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'adOt4NwaCM64cWQesnOl',
+#         'HOST': 'amordad-fei-service',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
