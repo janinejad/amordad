716eb4e9a52c6f8ef881d21a3b5358f3481ddfc4
janinejad <m.tarjomeh@gmail.com>
2025-04-26 19:43:10 +0330

    test register

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://amordadsteel.com', 'amordadsteel.com', 'http://amordadsteel.com']
+ALLOWED_HOSTS = ['https://amordadsteel.com', 'amordadsteel.com']
 # Application definition
 
 INSTALLED_APPS = [
