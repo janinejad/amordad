01d42742b23da900843d59f88924b22aa075e98b
janinejad <35501869+janinejad@users.noreply.github.com>
2025-06-27 14:25:11 +0330

    link Task changed to tasj

--- config/settings.py
+++ config/settings.py
@@ -18,18 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-
 DEBUG = False
-
 ALLOWED_HOSTS = ['https://amordadsteel.com', 'amordadsteel.com', 'http://amordadsteel.com']
 # ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
